d3e0fd9bf61355e2c105248749f4381b6c12ab51
David Wright <davidwright@utah.gov>
2020-06-23 07:30:29 -0600

    Change porfolio headline

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -30,7 +30,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Check Out Some of My Works.</h1>
+            <h1>Ask me about problems I've solved</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
